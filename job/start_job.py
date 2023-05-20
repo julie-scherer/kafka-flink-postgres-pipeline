dc8d336ec911e2f56c6bd510fee7f0f13e4c93a6
Zach Wilson <zachwilson130@gmail.com>
2023-05-20 12:48:43 -0700

    abstracting postgres values to environment

--- job/start_job.py
+++ job/start_job.py
@@ -14,52 +14,40 @@ source_ddl = f"""
         user_agent VARCHAR,
         host VARCHAR,
         ip VARCHAR,
-        event_time VARCHAR
+        headers VARCHAR,
+        event_time VARCHAR,
+        event_timestamp AS TO_TIMESTAMP(event_time),
+        -- declare time_ltz as event time attribute and use 5 seconds delayed watermark strategy
+        WATERMARK FOR event_timestamp AS event_timestamp - INTERVAL '15' SECOND
     ) WITH (
-                'connector' = 'kafka',
-              'properties.bootstrap.servers' = '{os.environ.get('KAFKA_URL')}',
-        'topic' = '{os.environ.get('KAFKA_TOPIC')}',
-     'properties.ssl.endpoint.identification.algorithm' = '',
-    'properties.group.id' = '{os.environ.get('KAFKA_GROUP')}',
-    'properties.security.protocol' = 'SSL',
-  'properties.ssl.truststore.location' = '/var/private/ssl/kafka_truststore.jks',
-  'properties.ssl.truststore.password' = '{os.environ.get("KAFKA_PASSWORD")}',
-  /* Configure the path of keystore (private key) if client authentication is required */
-  'properties.ssl.keystore.location' = '/var/private/ssl/kafka_client.jks',
-  'properties.ssl.keystore.password' = '{os.environ.get("KAFKA_PASSWORD")}',
-    'scan.startup.mode' = 'earliest-offset',
-'properties.auto.offset.reset' = 'earliest',
-      'format' = 'json'
-            )
+       'connector' = 'kafka',
+       'properties.bootstrap.servers' = '{os.environ.get('KAFKA_URL')}',
+       'topic' = '{os.environ.get('KAFKA_TOPIC')}',
+       'properties.ssl.endpoint.identification.algorithm' = '',
+       'properties.group.id' = '{os.environ.get('KAFKA_GROUP')}',
+       'properties.security.protocol' = 'SSL',
+       'properties.ssl.truststore.location' = '/var/private/ssl/kafka_truststore.jks',
+       'properties.ssl.truststore.password' = '{os.environ.get("KAFKA_PASSWORD")}',
+       'properties.ssl.keystore.location' = '/var/private/ssl/kafka_client.jks',
+       'properties.ssl.keystore.password' = '{os.environ.get("KAFKA_PASSWORD")}',
+       'scan.startup.mode' = 'earliest-offset',
+       'properties.auto.offset.reset' = 'earliest',
+       'format' = 'json'
+    )
 """
-print(source_ddl)
 
 table_env.execute_sql(source_ddl)
-file_sink_path = 'output'
-# Register the file sink table using CREATE TABLE syntax
-table_env.execute_sql(f"""
-    CREATE TABLE sink_table (
-         url STRING
-    ) WITH (
-        'connector' = 'filesystem',
-        'format' = 'csv',
-        'path' = '{file_sink_path}',
-        'csv.field-delimiter' = ','
-    );
-""")
 
 table_env.execute_sql(f"""
-
 CREATE TABLE processed_events (
    url VARCHAR
 ) WITH (
    'connector' = 'jdbc',
-   'url' = 'jdbc:postgresql://host.docker.internal:5432/postgres',
+   'url' = '{os.environ.get("POSTGRES_URL")}',
    'table-name' = 'processed_events',
-   'username' = 'zachwilson',
-    'password' = ''
+   'username' = '{os.environ.get("POSTGRES_USERNAME")}',
+   'password' = '{os.environ.get("POSTGRES_PASSWORD")}'
 );
-
 """)
 
 stmt_set = table_env.create_statement_set()
